d3ec0e8ee8930ec1d7fca0fa7745f833fa36d62b
Mahlet Seifu <akotet2123@gmail.com>
2024-01-28 07:27:55 +0300

    try

--- TaskTrackerApp/Showroom/models.py
+++ TaskTrackerApp/Showroom/models.py
@@ -9,7 +9,8 @@ class ApplicationListingCategory(TimestampsModel):
 
     def __str__(self):
         return self.name
-    
+
+
 class ApplicationListingRequest(TimestampsModel):
 
     ID_TYPE_CHOICES = [
@@ -30,6 +31,7 @@ class ApplicationListingRequest(TimestampsModel):
     def __str__(self) -> str:
         return 'Business Listing Vendor: ' + self.email.__str__()    
 
+
 class ApplicationListing(TimestampsModel):
     vendor = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(ApplicationListingCategory, on_delete=models.CASCADE)
@@ -47,12 +49,14 @@ class ApplicationListing(TimestampsModel):
     def __str__(self, *args, **kwargs):
         return self.name
 
+
 class ApplicationListingImage(TimestampsModel):
     image = models.FileField(upload_to='application_listing_images')
     listing = models.ForeignKey(ApplicationListing, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Image for {self.listing.name}"
+
 
 class ApplicationListingFile(TimestampsModel):
     file = models.FileField(upload_to='application_listing_files')
@@ -61,10 +65,12 @@ class ApplicationListingFile(TimestampsModel):
     def __str__(self):
         return f"File for {self.listing.name}"
 
+
 class ApplicationListingRating(TimestampsModel):
     listing = models.ForeignKey(ApplicationListing, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     value = models.PositiveSmallIntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(5)])
+
 
 class ApplicationListingReview(TimestampsModel):
     listing = models.ForeignKey(ApplicationListing, on_delete=models.CASCADE)
