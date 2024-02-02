00e6f04f004b90ae76b1e9f28a574bb7724fca2a
Mahlet Seifu <akotet2123@gmail.com>
2024-02-02 10:05:27 +0300

    endpoints

--- TaskTrackerApp/Showroom/urls.py
+++ TaskTrackerApp/Showroom/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import ApplicationListingView, ApplicationListingDetailView, ApplicationListingSearchView
+from .views import ApplicationListingView, ApplicationListingDetailView, ApplicationListingSearchView, AllApplicationListingView
 
 urlpatterns = [
     path('api/me/listings/', ApplicationListingView.as_view(), name='user-listings'),
     path('api/listings/<int:pk>/', ApplicationListingDetailView.as_view(), name='listing-detailview'),
     path('api/listings/search/', ApplicationListingSearchView.as_view(), name='listing-search'),
+    path('api/listings/', AllApplicationListingView.as_view(), name='listing-all'),
 ]
 
